d71bffacbe4a957c022f7e2b65a8feb32b5e1667
descon-uccs <pbrown2@uccs.edu>
2024-03-05 13:13:31 -0700

    another Phi

--- sandbox.py
+++ sandbox.py
@@ -9,10 +9,16 @@ from potential_game import *
 from tabulate import tabulate
 
 if __name__ == "__main__" :
+    # Phi = [[10,-1,-1,-1],
+    #        [8,6,-1,-1],
+    #        [-1,5,5,5],
+    #        [-1,-1,5,5]]
+    
     Phi = [[10,6,-1],
-           [8,6,-1],
-           [-1,4,7]]
-           # [-1,-1,5]]
+           [8,6,4],
+           [4,4,2]]
+           # [-1,-1,5,5]]
+    
     Phi = np.array(Phi)
     
 
